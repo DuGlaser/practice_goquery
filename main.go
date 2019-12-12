8c4e4b1748f3694db89e2c83551dbb171bf10261
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-12-12 13:06:58 +0900

    :sparkles:ADD serch nino img func
    ~

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"net/http"
+	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -13,19 +15,30 @@ func ExampleScrape() {
 }
 
 func main() {
-	res, err := http.Get("https://golang.org/")
+	// file, err := os.OpenFile("result.html", os.O_RDONLY|os.O_CREATE, 0666)
+
+	doc, err := goquery.NewDocument("https://www.google.com/search?q=%E4%B8%AD%E9%87%8E%E4%BA%8C%E4%B9%83&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjYy6_egK_mAhXaMt4KHd0NBe4Q_AUoAXoECAoQAw&biw=412&bih=766")
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Print("url scarapping failed")
 	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s\n", res.StatusCode, res.Status)
-	}
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	res, err := doc.Find("body").Html()
 	if err != nil {
-		log.Println(err)
+		fmt.Print("dom get failed")
+	}
+	ioutil.WriteFile("./result.html", []byte(res), os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	section, _ := doc.Find("iframe").Attr("src")
-	fmt.Println(section)
+	fileInfos, _ := ioutil.ReadFile("./result.html")
+	stringReader := strings.NewReader(string(fileInfos))
+	doc, err = goquery.NewDocumentFromReader(stringReader)
+	if err != nil {
+		fmt.Print("url scarapping failed")
+	}
+	// // Load the HTML document
+
+	doc.Find("img").Each(func(index int, s *goquery.Selection) {
+		fmt.Println(s.Attr("src"))
+	})
 }
